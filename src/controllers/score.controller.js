5d45d3c8888749a41735908576b89347516c50d8
govind singh gurjar <gurjargovind994@gmail.com>
2024-09-24 18:30:33 +0530

    added update score

--- src/controllers/score.controller.js
+++ src/controllers/score.controller.js
@@ -13,9 +13,59 @@ export const addScore = async (req, res) => {
 
 export const getLeaderboard = async (req, res) => {
   try {
-    const scores = await Score.find().sort({ score: -1 }).limit(10);
+    const {quizId}=req.body;
+    if(!quizId){
+      throw new Error("quizId is required ");
+      
+    }
+
+    const scores = await Score.find({"quizId":quizId}).sort({ score: -1 }).limit(10);
     res.status(200).json(scores);
   } catch (error) {
+    console.log(error);
+    
     res.status(500).send('Error fetching leaderboard');
   }
 };
+
+export const updateScore = async(req,res)=>{
+  try {
+    const {scoreId,score}=req.body;
+    if(!scoreId){
+      throw new Error("quizId is required ");
+    }
+    if(!score){
+      throw new Error("score is required ");
+    }
+    const updatedData = {
+      score
+    }
+    const updatedScore = await Score.findByIdAndUpdate(
+      scoreId,
+      updatedData,
+      {
+        new: true,
+      });
+
+      res.status(200).json(updatedScore);
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error fetching leaderboard');
+  }
+}
+
+export const getScore= async(req,res)=>{
+  try {
+    const {scoreId}=req.body;
+    if(!scoreId){
+      throw new Error("quizId is required ");
+    };
+  
+    const score = await Score.findById(scoreId);
+    res.status(200).json(score);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error fetching leaderboard');
+  }
+}
--- src/routes/score.routes.js
+++ src/routes/score.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express"
-import { addScore, getLeaderboard } from '../controllers/score.controller.js';
+import { addScore, getLeaderboard,getScore,updateScore } from '../controllers/score.controller.js';
 const router = Router()
 router.route("/").post(addScore)
 router.route("/").get(getLeaderboard)
+router.route("/points").get(getScore)
+router.route("/points").post(updateScore)
 export default router
